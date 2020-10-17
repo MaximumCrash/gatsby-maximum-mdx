140a9b689d4cf4e05f0a8a1558d99aed7a089e5f
Rejon <rejon@maximumcrash.com>
2020-10-17 12:27:34 -0400

    Copied over community-portal project

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# MaximumMDX
-A gatsby starter for MDX dependent projects that have a need for 
+# Community Development at Maker
+
+The Community Development group's mandate is to directly foster the growth of the MakerDAO ecosystem by incentivizing our most motivated members. We do this through a series of initiatives that encourage people to participate in, and contribute to, the success of the Dai Stablecoin platform.
+
+## Our Initiatives
+
+### [Community Meetup Grants](meetups/README.md)
+
+Community members play an invaluable role in building a stronger MakerDAO ecosystem. The Community Development Group provides the resources needed to increase Dai adoption and help MakerDAO bring stability and financial inclusion to the world. To accomplish these goals, we created a 'Do It Yourself' Meetup guide and microgranting program dedicated to helping anyone create and organize a local community meetup.
+
+### [Community Translations](translations/README.md)
+
+We bring MakerDAO educational content to people around the world in their native languages by incentivizing our community translators with Dai bounties.
+
+### [Development Grants Program](grants/README.md)
+
+The MakerDAO Community grants program provides funding for projects that increase Dai adoption, support emerging economies, open new markets, provide novel uses for the stablecoin, or otherwise further the principles of the MakerDAO Foundation.
+
+### [Hackathons](hackathons/README.md)
+
+MakerDAO participates in a number of Hackathons every year. This section contains some information about how to request our participation in an upcoming event.
+
+### [Contributing](contributing/README.md)
+
+If you have any suggestions about how you think these initiatives should work, could be improved, or if you are interested in helping out, please join our [#community-development](https://chat.makerdao.com/channel/community-development) channel and introduce yourself.
+
+## Our Resources
+
+### [Awesome-MakerDAO](https://github.com/makerdao/awesome-makerdao/)
+
+Awesome-MakerDAO is a community-curated list of MakerDAO-related resources and content to which anyone can contribute. It contains official documentation, beginner guides, in-depth analysis, links to 3rd party tools, partnership announcements, AMAs, podcasts, and much more.
+
+### [MakerDAO MCD FAQs](faqs/README.md)
+
+This section provides answers to commonly asked questions about everything MakerDAO.
+
+We welcome you to help improve the MakerDAO FAQs by submitting Pull Requests or reaching out to us with feedback.
+
+### [MakerDAO SCD FAQs](scd-faqs/README.md)
+
+This section provides answers to commonly asked questions about everything MakerDAO, specifically as it related to the Single-Collateral Dai version of the system.
+
+We welcome you to help improve the MakerDAO FAQs by submitting Pull Requests or reaching out to us with feedback.
+
+### [Governance](governance/README.md)
+
+To ensure good governance and accountability, we've set up this section to contain helpful resources such as transcripts of the meetings, historical summaries, and more.
+
+### [Onboarding Guides](onboarding/README.md)
+
+These are community-built guides to help onboard different types of users into our ecosystem. Whether you're a potential MKR voter, a Vault owner, or Dai user, this section should have something for you. If it doesn't, reach out to us in our [chat](https://chat.makerdao.com/channel/community-development) or [forums](https://forum.makerdao.com/). This section has a handful of guides _coming soon™️._
---
+++ gatsby-node.js
@@ -0,0 +1,130 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+const { UrlConverter } = require("./src/build-utils");
+const FALLBACK_LOCALE = "en";
+//TODO(Rejon): Add in support for the case similar pages exist outside of the locale folders.
+//			   We don't want to override pages at the top level if they exist.
+
+//Create redirect fallbacks to default locales.
+//ie If the user types /contribute and it exists in our default locale, take us to that page.
+exports.createPages = async ({ graphql, actions }) => {
+  const { createRedirect } = actions; //actions is collection of many actions - https://www.gatsbyjs.org/docs/actions
+  const { data: pages } = await graphql(`
+    query redirects {
+      pages: allMdx(
+        filter: {
+          fileAbsolutePath: {
+            regex: "//([\\\\w]{2})/(?!header.mdx|index.mdx|sidenav.mdx|example.mdx|footer.mdx|404.mdx|.js|.json)/"
+          }
+        }
+      ) {
+        edges {
+          node {
+            fileAbsolutePath
+          }
+        }
+      }
+    }
+  `);
+
+  pages.pages.edges.map(({ node }) => {
+    const noLocalePath = UrlConverter(node)
+                            .replace(/^\/([\w]{2})\//, "/")
+                            .replace('index', '');
+
+    createRedirect({
+      fromPath: noLocalePath,
+      toPath: `/${FALLBACK_LOCALE}${noLocalePath}`,
+      isPermanent: true,
+    });
+  });
+
+  //Legacy Redirect Fix
+  createRedirect({
+    fromPath: '/makerdao-mcd-faqs/faqs',
+    toPath: `/${FALLBACK_LOCALE}/faqs/`
+  });
+
+  createRedirect({
+    fromPath: '/governance',
+    toPath: `/${FALLBACK_LOCALE}/learn/governance/`
+  });
+
+
+  createRedirect({
+    fromPath: '/contributing',
+    toPath: `/${FALLBACK_LOCALE}/contribute/`
+  });
+
+  createRedirect({
+    fromPath: '/grants',
+    toPath: `/${FALLBACK_LOCALE}/funding/development-grants`
+  })
+
+  createRedirect({
+    fromPath: '/meetups',
+    toPath: `/${FALLBACK_LOCALE}/funding/meetup-funding`
+  })
+
+  createRedirect({
+    fromPath: '/meetups/requesting-funds',
+    toPath: `/${FALLBACK_LOCALE}/funding/meetup-funding`
+  })
+
+  createRedirect({
+    fromPath: '/hackathons',
+    toPath: `/${FALLBACK_LOCALE}/funding/hackathon-funding`
+  })
+
+  createRedirect({
+    fromPath: '/risk',
+    toPath: `/${FALLBACK_LOCALE}/learn/collateral-and-risk/`
+  })
+
+  createRedirect({
+    fromPath: '/translations',
+    toPath: `/${FALLBACK_LOCALE}/contribute/translations`
+  })
+
+  createRedirect({
+    fromPath: '/onboarding',
+    toPath: `/${FALLBACK_LOCALE}/learn`
+  })
+
+  createRedirect({
+    fromPath: '/onboarding/vault-onboarding',
+    toPath: `/${FALLBACK_LOCALE}/learn/vaults/vaults-tutorial`
+  })
+
+  createRedirect({
+    fromPath: '/onboarding/voter-onboarding',
+    toPath: `/${FALLBACK_LOCALE}/learn/governance/how-voting-works`
+  })
+
+  createRedirect({
+    fromPath: '/governance',
+    toPath: `/${FALLBACK_LOCALE}/learn/governance/`
+  })
+  createRedirect({
+    fromPath: '/governance/common-topics',
+    toPath: `/${FALLBACK_LOCALE}/learn/governance/common-topics`
+  })
+};
+
+exports.onCreatePage = async ({page, pathPrefix, actions}) => {
+  const {createPage, deletePage} = actions;
+
+  // inject breadcrumbs into page context
+  const { context: oldPageContext } = page
+  deletePage(page)
+  createPage({
+    ...page,
+    context: {
+      ...oldPageContext,
+      pagePath: page.path //NOTE(Rejon): I provide this so we can have a navigational anchor during static builds for pathDirs and sidenav/breadcrumb data.
+    }
+  });
+}
